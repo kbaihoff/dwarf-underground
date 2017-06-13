1c3be31fd5f738993c4534501a920d6bd2ad8c39
Kai Hoffman <hoffm154@purdue.edu>
2017-06-13 14:39:35 -0400

    Break off HeaderClearfix component from Header component

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import './Header.css';
+import HeaderClearfix from './HeaderClearfix';
 
 class Header extends React.Component {
   render() {
     return (
       <div className="expanded row header">
-        <div className="clearfix">
-          <div className="float-left logo">
-            <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
-          </div>
-          <div className="float-right nav-links">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Log in</a>
-          </div>
-        </div>
+        <HeaderClearfix />
         <div className="headline text-center small-12 columns">
           <h1>The Dwarf Underground</h1>
           <h3>Delving deep for the <span className="gold">shiniest</span> news</h3>
---
+++ src/HeaderClearfix.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './HeaderClearfix.css';
+
+class HeaderClearfix extends React.Component {
+  render() {
+    return (
+      <div className="clearfix">
+        <div className="float-left logo">
+          <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
+        </div>
+        <div className="float-right nav-links">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Log in</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HeaderClearfix
