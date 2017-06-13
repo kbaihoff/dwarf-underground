04b12cd0a21db44bd5db2c1b7b2477782a4b45db
Kai Hoffman <hoffm154@purdue.edu>
2017-06-13 16:24:44 -0400

    Make comment field show on Comments click

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -3,20 +3,30 @@ import './ArticleLinks.css';
 
 class ArticleLinks extends React.Component {
   clickComment(ev) {
-    console.log(ev.target.parentElement)
+    ev.target.closest('.article-links').nextElementSibling.style.display = 'inline'
+    console.log(ev.target.closest('.article-links').nextElementSibling.style)
   }
   
   render() {
     return (
-      <div className="article-links">
-        <a className="article-link" onClick={this.clickComment.bind(this)}>
-          <i className="fa fa-comments-o"></i>
-          <span className="article-link-text">Comments</span>
-        </a>
-        <a className="article-link" href="#">
-          <i className="fa fa-share"></i>
-          <span className="article-link-text">Share Post</span>
-        </a>
+      <div>
+        <div className="article-links">
+          <a className="article-link" onClick={this.clickComment.bind(this)}>
+            <i className="fa fa-comments-o"></i>
+            <span className="article-link-text">Comments</span>
+          </a>
+          <a className="article-link" href="#">
+            <i className="fa fa-share"></i>
+            <span className="article-link-text">Share Post</span>
+          </a>
+        </div>
+        <div className="commenting">
+          <input type="text" className="comment-field" placeholder="Comment here..." />
+          <button type="submit" className="comment-button">Sayeth</button>
+          <ul className="no-bullet">
+            <li>Test</li>
+          </ul>
+        </div>
       </div>
     )
   }
