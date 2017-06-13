fcc4ab21595745706417c8b598f82194a4f909a1
Kai Hoffman <hoffm154@purdue.edu>
2017-06-13 15:34:04 -0400

    Break off ShareLink and CommentLink components from ArticleLinks component, and render OtherArticles using map

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './ArticleLinks.css';
+import CommentLink from './CommentLink'
+import ShareLink from './ShareLink'
 
 class ArticleLinks extends React.Component {
   render() {
     return (
       <div className="article-links">
-        <a className="article-link" href="#">
-          <i className="fa fa-comments-o"></i>
-          <span className="article-link-text">Comments</span>
-        </a>
-        <a className="article-link" href="#">
-          <i className="fa fa-share"></i>
-          <span className="article-link-text">Share Post</span>
-        </a>
+        <CommentLink />
+        <ShareLink />
       </div>
     )
   }
